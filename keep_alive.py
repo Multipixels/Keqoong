ce87f9b7271493b20bb7e1af0303da2da932ac97
Richard <75509898+Multipixels@users.noreply.github.com>
2021-09-25 23:21:46 +0000

    Changed title

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-  return "Hello"
+  return "Running"
 
 def run():
   app.run(host='0.0.0.0',port=8080)
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 import discord.ext
 from discord.utils import get
 from discord.ext import commands, tasks
-from discord.ext.commands import has_permissions,  CheckFailure, check
+from discord.ext.commands import has_permissions, CheckFailure, check
 from keep_alive import keep_alive
 import asyncio
 #^ basic imports for other features of discord.py and python ^
@@ -15,8 +15,6 @@ client = discord.Client()
 emojis = ['▶️', '⏩']
 imageUrls = ["https://oyster.ignimgs.com/mediawiki/apis.ign.com/genshin-impact/a/a1/Genshin_Impact_20200929215315.jpg?width=1280," ,"https://www.pcinvasion.com/wp-content/uploads/2020/10/Genshin-Impact-wishes-microtransactions-currencies-guide-1a.jpg","https://static1.thegamerimages.com/wordpress/wp-content/uploads/2020/10/Screenshot-2565.png?q=50&fit=crop&w=1920&dpr=1.5" ,"https://www.pcinvasion.com/wp-content/uploads/2020/10/Gen-pct-char-5.jpg" ,"https://s.yimg.com/ny/api/res/1.2/Q3aQzxtxrTn13fsNF_Vrug--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTM1OS45MzM0NDQyNTk1Njc0/https://s.yimg.com/os/creatr-uploaded-images/2021-05/0284e1f0-b15d-11eb-beaf-42b099ca5a0c" ,"https://i.ytimg.com/vi/nZS7cIcWIlw/hqdefault.jpg" ,"https://static1.thegamerimages.com/wordpress/wp-content/uploads/2020/10/pjimage-2020-10-21T120904.306.jpg" ,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOGMA41vsSPDOCF012lAaHiYiqbAm7prFNcQ&usqp=CAU" ,"https://www.alphr.com/wp-content/uploads/2021/06/Xiao-Wish-Art.jpg" ,"https://cdn.gamerstopia.com/wp-content/uploads/Chonyun-Genshin-Impact-Character-Profile-Featured-Image.jpg"]
 fullWishUrl = "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2020/09/genshin-impact-wishes.jpg"
-
-lantern = "\595733537651687426"
 
 #Prefix
 client = commands.Bot(command_prefix = '+')
@@ -29,7 +27,7 @@ async def on_ready():
 @client.command()
 async def wish(ctx):
     wishNumber = 0
-    embed=discord.Embed(title="NYOOOOOOOOOOM", description="IM A TRAIN", color=0x3ba4eb)
+    embed=discord.Embed(title="🌠 Genshin Wish Simulator 🌠", description="IM A TRAIN", color=0x3ba4eb)
     embed.set_image(url=imageUrls[wishNumber])
     message = await ctx.send(embed=embed)
 
@@ -37,6 +35,7 @@ async def wish(ctx):
       await message.add_reaction(emoji)
 
     def check(reaction, user):
+      print("test")
       return user == ctx.author and str(reaction.emoji) == '▶️'
 
     for loop in range(10):
